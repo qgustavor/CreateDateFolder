1edbf02b8afece1ca5c64111db13c38ac159c1c6
Gustavo Rodrigues <qgustavor@users.noreply.github.com>
2025-05-02 17:34:38 -0300

    Replace AI slop with AI slop
    ChatGPT, when I asked it to generate a zip of the repository, made up a new code instead of copying the correct one from the context window. Sure, after that there was so much context that no transformer model could just infer that they should copy that. Anyway, that's AI slop because I'm being lazy to install a C compiler or care to program it myself using a scripting language. Sure, one of the reasons I want a .exe is in case a scripting language would be blocked (which could be the case if I wrote a PowerShell script).

--- src/main.c
+++ src/main.c
@@ -1,23 +1,25 @@
+#include <stdio.h>
+#include <time.h>
+#include <string.h>
 #include <windows.h>
 
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+int main(int argc, char *argv[]) {
+    if (argc < 2) return 1;
+
     time_t t = time(NULL);
     struct tm tm;
     localtime_s(&tm, &t);
 
-    char folderName[MAX_PATH];
-    snprintf(folderName, sizeof(folderName), "%02d-%02d-%02d",
+    char dateFolder[16];
+    snprintf(dateFolder, sizeof(dateFolder), "%02d-%02d-%02d",
              tm.tm_year % 100, tm.tm_mon + 1, tm.tm_mday);
 
-    char basePath[MAX_PATH];
-    GetModuleFileNameA(NULL, basePath, MAX_PATH);
-    char *lastSlash = strrchr(basePath, '\');
-    if (lastSlash) *lastSlash = '\0';
-
     char fullPath[MAX_PATH];
-    snprintf(fullPath, sizeof(fullPath), "%s\\%s", basePath, folderName);
+    snprintf(fullPath, sizeof(fullPath), "%s\\%s", argv[1], dateFolder);
 
-    CreateDirectoryA(fullPath, NULL);
+    if (!CreateDirectoryA(fullPath, NULL)) {
+        if (GetLastError() != ERROR_ALREADY_EXISTS) return 1;
+    }
 
     return 0;
 }
