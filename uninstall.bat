a37919d255c8a7fb2722bb65b379962415b55c7d
qgustavor <qgustavor@users.noreply.github.com>
2025-05-06 10:52:13 -0300

    Fix installer
    Now requires admin permissions

--- uninstall.bat
+++ uninstall.bat
@@ -1,8 +1,8 @@
 @echo off
 setlocal
 
-reg delete "HKCU\Directory\Background\shell\CreateDateFolder\command" /f
-reg delete "HKCU\Directory\Background\shell\CreateDateFolder" /f
+reg delete "HKCR\Directory\Background\shell\CreateDateFolder\command" /f
+reg delete "HKCR\Directory\Background\shell\CreateDateFolder" /f
 
 echo.
 echo Aplicativo deinstalado com sucesso
